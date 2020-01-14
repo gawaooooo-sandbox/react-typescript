6ddee2abb76068fc0ee5b8a443e857f205b611a2
gawao <gawao57@gmail.com>
2020-01-14 23:51:14 +0900

    7-3 p.92

--- 07-component/03-localstate/README.md
+++ 07-component/03-localstate/README.md
@@ -7,3 +7,15 @@
     - コンポーネント自身が内部に持つ状態のこと
 - ライフサイクル
 
+
+
+## Local State
+- ComponentにLocal Stateの型を渡す
+- 初期化はconstructorで
+- 値の設定は必ず `setState()` メソッドで
+  - 引数
+    - 設定したい値のStateオブジェクト
+      - Stateに固定値を設定するとき
+    - `(prevState, props) => newState` 形式のStateとPropsを引数として受け取り新しいStateを返す関数
+      - 動的に変更したい場合
+
--- 07-component/03-localstate/src/App.tsx
+++ 07-component/03-localstate/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, SyntheticEvent } from 'react';
 import { Button, Card, Statistic } from 'semantic-ui-react';
 
 import './App.css';
@@ -8,17 +8,24 @@ interface AppState {
 }
 
 // Componentクラスの型引数にはデフォルト値として `{}` という空オブジェクトが設定されている
+// AppStateはLocal Stateの型になる
 class App extends Component<{}, AppState> {
   constructor(props: {}) {
     super(props);
+    // Local Stateの初期化に必要なのでconstructorあり
     this.state = { count: 0 };
   }
 
-  increment() {
+  // アロー関数にすると、 `this.increment` で呼び出せる
+  increment = (e: SyntheticEvent) => {
+    // SyntheticEvent -> イベントハンドラのオブジェクト
+    e.preventDefault(); // 該当要素のオリジナルなonClickの挙動を抑制
+
+    // this.stateに直接値が導入できない
     this.setState(prevState => ({
       count: prevState.count + 1,
     }));
-  }
+  };
 
   decrement() {
     this.setState(prevState => ({
@@ -41,10 +48,11 @@ class App extends Component<{}, AppState> {
           </Statistic>
           <Card.Content>
             <div className="ui two buttons">
+              {/* 引数なしにメンバーメソッドのdecrement()を実行するアロー関数リテラル */}
               <Button color="red" onClick={() => this.decrement()}>
                 -1
               </Button>
-              <Button color="green" onClick={() => this.increment()}>
+              <Button color="green" onClick={this.increment}>
                 +1
               </Button>
             </div>
